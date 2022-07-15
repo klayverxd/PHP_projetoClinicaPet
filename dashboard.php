4ae97c6708e339a73148d4a39022e68bbf6bc4ec
Klayver Ximenes <klayverx@gmail.com>
2022-07-14 22:42:35 -0300

    feat: consulta banco na dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,7 @@
+<?php
+include 'classes/Cliente.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -346,6 +350,15 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+
+                        <h1>Quantidade de clientes clientes:
+                            <?php
+                            $objCliente = new Cliente();
+                            $objCliente->verificaQtdClientes();
+
+                            echo $objCliente->qtdClientes->fetch_object()->qtd;
+                            ?>
+                        </h1>
 
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
